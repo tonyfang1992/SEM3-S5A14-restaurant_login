9edba85c91815d95fe0a495cd637d712091f30a1
wendy03 <wendysu03@gmail.com>
2019-11-21 21:16:48 +0800

    Add user authentication in todo and home routers

---
+++ config/passport.js
@@ -0,0 +1,106 @@
+const LocalStrategy = require('passport-local').Strategy
+const FacebookStrategy = require('passport-facebook').Strategy
+const GitHubStrategy = require('passport-github').Strategy
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+
+
+// 載入 User model
+const User = require('../models/user')
+module.exports = passport => {
+  passport.use(
+    new LocalStrategy({ usernameField: 'email', passReqToCallback: true }, (req, email, password, done) => {
+      User.findOne({
+        email: email
+      }).then(user => {
+        if (!user) {
+          return done(null, false, req.flash('warning_msg', '輸入的email未註冊'))
+        }
+        bcrypt.compare(password, user.password, (err, isMatch) => {
+          if (err) throw err
+          if (isMatch) {
+            return done(null, user)
+          } else {
+            return done(null, false, req.flash('warning_msg', 'Email/Password 錯誤!'))
+          }
+        })
+      })
+    })
+  )
+
+  passport.use(
+    new FacebookStrategy({
+      clientID: process.env.FACEBOOK_ID,
+      clientSecret: process.env.FACEBOOK_SECRET,
+      callbackURL: process.env.FACEBOOK_CALLBACK,
+      profileFields: ['email', 'displayName']
+    }, (accessToken, refreshToken, profile, done) => {
+      User.findOne({
+        email: profile._json.email
+      }).then(user => {
+        if (!user) {
+          var randomPassword = Math.random().toString(36).slice(-8)
+          bcrypt.genSalt(10, (err, salt) =>
+            bcrypt.hash(randomPassword, salt, (err, hash) => {
+              var newUser = User({
+                name: profile._json.name,
+                email: profile._json.email,
+                password: hash
+              })
+              newUser.save().then(user => {
+                return done(null, user)
+              }).catch(err => {
+                console.log(err)
+              })
+            })
+          )
+        } else {
+          return done(null, user)
+        }
+      })
+    })
+  )
+
+  passport.use(
+    new GitHubStrategy({
+      clientID: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+      callbackURL: process.env.GITHUB_CALLBACK,
+      profileFields: ['email', 'displayName']
+    }, (accessToken, refreshToken, profile, done) => {
+      User.findOne({
+        email: profile._json.login
+      }).then(user => {
+        if (!user) {
+          const randomPassword = Math.random().toString(36).slice(-8)
+          const userEmail = `${profile._json.login}@githoub.com`
+          bcrypt.genSalt(10, (err, salt) =>
+            bcrypt.hash(randomPassword, salt, (err, hash) => {
+              var newUser = User({
+                name: profile._json.name,
+                email: userEmail,
+                password: hash
+              })
+              newUser.save().then(user => {
+                return done(null, user)
+              }).catch(err => {
+                console.log(err)
+              })
+            })
+          )
+        } else {
+          return done(null, user)
+        }
+      })
+    })
+  )
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id)
+  })
+  passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+      done(err, user)
+    })
+  })
+}
